9d3141db4fd348b553fbee2d58097a2f72914736
Alpvax <alpvax@gmail.com>
2019-01-04 15:56:36 +0000

    Fix ContextMenu

--- index.js
+++ index.js
@@ -1,51 +1,25 @@
 import ContextMenu from "./ContextMenu";
 
+const defaultOptions = {
+  defaultFuncName: "contextMenuItems"
+};
+
 export default {
   install(Vue, opts) {
     // Merge options argument into options defaults
-    const options = opts; //TODO: {...optionsDefaults, ...opts };
-
-    // Create plugin's root Vue instance
-    const root = new Vue({
-      data: { targets: options.data.targets },
-      render: createElement => createElement(ContextMenu)
-    });
-
-    // Mount root Vue instance on new div element added to body
-    root.$mount(document.body.appendChild(document.createElement("div")));
-
-    function showMenuHandler(vm, buildFunc, bubble) {
-      return function(event) {
-        let pos = { x: event.x, y: event.y };
-        let items = buildFunc(vm, {event, items: []});
-        if (bubble) {
-          let comp = vm.$parent;
-          while (comp) {
-            /*let e = comp.contextMenuItems(comp, items, event) || [];
-            items = [].concat(items, e);*/
-            console.log(comp); //XXX
-            comp = comp.$parent;
-          }
-        }
-        root.data.items = items;
-        this.$nextTick(() => root.data.pos = pos);
-      };
-    }
+    const options = {...defaultOptions, ...opts };
 
     Vue.mixin({
       methods: {
-        contextMenuItems(vm, menuitems, event) {
-          return menuitems.map((v) => v.text).includes("WhoAmI?") ? [] : [{text: "WhoAmI?", call(){console.log(vm);}}];
-        },
         hideContextMenu() {
-          root.data.items = [];
+          root.$data.items = [];
         }
       }
     });
     Vue.directive("contextmenu", {
       bind(el, binding, vnode) {
         let build = binding.value;
-        if (build) {
+        if (build !== undefined) {
           if (typeof build !== "function") { // If not a function,
             if (typeof build[Symbol.iterator] === "function") { // Is it iterable?
               build = (vm, args) => [].concat(build);
@@ -53,10 +27,49 @@ export default {
               Vue.util.warn(`v-${binding.name}="${binding.expression}" expects either a function value or an iterable value. Recieved: ${build}`);
             }
           }
+        } else {
+          build = (vm, args) => vm[options.defaultFuncName](vm, args)
         }
         let bubble = !binding.modifiers.stop;
         el.addEventListener("contextmenu", showMenuHandler(vnode, build, bubble), false);
       }
     });
+
+    // Create plugin's root Vue instance
+    const root = new Vue({
+      data: {
+        items: [],
+        pos: {
+          x: 0,
+          y: 0
+        }
+      },
+      components: {ContextMenu},
+      template: `<ContextMenu
+        :items="items"
+        :pos="pos"
+      />`
+    });
+
+    // Mount root Vue instance on new div element added to body
+    root.$mount(document.body.appendChild(document.createElement("div")));
+
+    let contextEvent;
+    function showMenuHandler(vnode, buildFunc, bubble) {
+      return function(event) {
+        if (event !== contextEvent) {
+          contextEvent = event;
+          root.$data.pos = { x: event.x, y: event.y };
+          root.$data.items = [];
+          console.log(root.$data)
+        }
+        event.preventDefault()
+        if (!bubble) {
+          event.stopPropagation()
+        }
+        let items = buildFunc(vnode.context, {event, menuitems: root.$data.items});
+        root.$data.items.push(...items);
+      };
+    }
   }
 };
