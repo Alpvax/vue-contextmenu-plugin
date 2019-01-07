9b10ad2dc290e61adcdf06fd5152a16404a9e7af
Alpvax <alpvax@gmail.com>
2019-01-07 18:00:47 +0000

    Add .directive modifiers: stop, .noprevent, .self, .capture and .once

--- index.js
+++ index.js
@@ -13,28 +13,53 @@ export default {
       root.$data.items = [];
     }
 
+    function bindEventListener(el, binding, vnode) {
+      let build = binding.value;
+      if (build !== undefined) {
+        if (typeof build !== "function") { // If not a function,
+          if (typeof build[Symbol.iterator] === "function") { // Is it iterable?
+            build = (vm, args) => [].concat(build);
+          } else {
+            Vue.util.warn(`v-${binding.name}="${binding.expression}" expects either a function value or an iterable value. Recieved: ${build}`);
+          }
+        }
+      } else {
+        build = (vm, args) => vm[options.defaultFuncName](vm, args);
+      }
+      let modifiers = Object.freeze(Object.assign({}, binding.modifiers, {
+        prevent: !binding.modifiers.noprevent
+      }));
+      let listener = showMenuHandler(vnode, build, modifiers);
+      listener.opts = Object.freeze({
+        capture: !!binding.modifiers.capture,
+        once: !!binding.modifiers.once
+      });
+      el["vue-contextmenu-plugin-eventListener"] = listener;
+      el.addEventListener("contextmenu", listener, listener.opts);
+    }
+
+    function unbindEventListener(el, binding) {
+      let listener = el["vue-contextmenu-plugin-eventListener"];
+      el.removeEventListener("contextmenu", listener, listener.options);
+    }
+
     Vue.mixin({
       methods: {
         hideContextMenu
       }
     });
     Vue.directive("contextmenu", {
-      bind(el, binding, vnode) {
-        let build = binding.value;
-        if (build !== undefined) {
-          if (typeof build !== "function") { // If not a function,
-            if (typeof build[Symbol.iterator] === "function") { // Is it iterable?
-              build = (vm, args) => [].concat(build);
-            } else {
-              Vue.util.warn(`v-${binding.name}="${binding.expression}" expects either a function value or an iterable value. Recieved: ${build}`);
-            }
-          }
-        } else {
-          build = (vm, args) => vm[options.defaultFuncName](vm, args);
+      bind: bindEventListener,
+      update(el, binding, vnode, oldVnode) {
+        if (vnode !== oldVnode) {
+          unbindEventListener(el, binding, oldVnode);
+          bindEventListener(el, binding, vnode);
+        } else if (binding.value !== binding.oldValue) {
+          unbindEventListener(el, binding, vnode);
+          bindEventListener(el, binding, vnode);
         }
-        let bubble = !binding.modifiers.stop;
-        el.addEventListener("contextmenu", showMenuHandler(vnode, build, bubble), false);
-      }
+      },
+      unbind: unbindEventListener
     });
 
     // Create plugin's root Vue instance
@@ -58,20 +83,40 @@ export default {
     document.addEventListener("click", hideContextMenu, false);
 
     let contextEvent;
-    function showMenuHandler(vnode, buildFunc, bubble) {
+    function showMenuHandler(vnode, buildFunc, modifiers) {
       return function(event) {
+        if (modifiers.self && event.target !== event.currentTarget){
+          return;
+        }
         if (event !== contextEvent) {
           contextEvent = event;
           root.$data.pos = { x: event.x, y: event.y };
           root.$data.items = [];
           //console.log(root.$data);
         }
-        event.preventDefault();
-        if (!bubble) {
+        if (modifiers.prevent) {
+          event.preventDefault();
+        }
+        if (stop) {
           event.stopPropagation();
         }
-        let items = buildFunc(vnode.context, {event, menuitems: root.$data.items});
-        root.$data.items.push(...items);
+        let targetEl = event.target;
+        while (!targetEl.hasOwnProperty("__vue__")) {
+          targetEl = targetEl.parentElement;
+        }
+        let args = {
+          event,
+          menuitems: root.$data.items,
+          targetVm: targetEl.__vue__,
+          targetEl: event.target,
+          directiveModifiers: modifiers
+        };
+        let items = buildFunc(vnode.context, args);
+        if(items && typeof items[Symbol.iterator] === "function") {
+          root.$data.items.push(...items);
+        } else {
+          console.warn("Context menu build function did not return an iterable value:", items, "\nVNode:", vnode);
+        }
       };
     }
   }
